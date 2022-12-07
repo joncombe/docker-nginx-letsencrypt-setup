ec8c9f7724d30e50ce7a3ed3444f877286ecdca9
Jon Combe <github@joncombe.net>
2022-12-07 10:35:56 +0700

    Copy in the meat and potatoes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# docker-nginx-letsencrypt-setup
+This script sets up a Let's Encrypt certificate on an Nginx webserver for use in a Docker environment. It only takes a minute.
+
+There is an _excellent_ blog post on this topic, [HTTPS using Nginx and Let's encrypt in Docker](https://mindsers.blog/post/https-using-nginx-certbot-docker/), which has helped me many, many times (_thank you, Nathanaël_). This script goes through very similar steps to what is described in that blog post, but in a completely automated way.
+
+> This script has been tested on Ubuntu servers. I think it will work on any Linux environment with Docker installed but I'd appreciate your feedback on that.
+
+To get started, be sure that your DNS already is configured and has propagated. `ssh` into your target server and copy the `certbot.py` and `certbot.json` files from this repo to your directory.
+
+Here, let me help you:
+
+```
+curl -o certbot.py https://raw.githubusercontent.com/joncombe/docker-nginx-letsencrypt-setup/main/certbot.py
+curl -o certbot.json https://raw.githubusercontent.com/joncombe/docker-nginx-letsencrypt-setup/main/certbot.json
+```
+
+Next, edit the `certbot.json` file:
+
+- `domain` should be the FQDN of your website, e.g. "en.wikipedia.org"
+- `volume_prefix` is the local path to the location your persisted docker data files are stored. If you are not sure what to do here, leave it as it is. _[TODO: improve how to explain this]_.
+- `email` is the email address you provide to Let's Encrypt
+
+> Be aware, running this next command assumes you agree to the Let's Encrypt Terms of Service. See: https://letsencrypt.org/documents/LE-SA-v1.3-September-21-2022.pdf. If you do not agree to these rules, please do NOT use this script.
+
+Once this is done, get your certificate:
+
+```
+python3 certbot.py
+```
+
+All done. There, wasn't that easy?
+
+At the end of it all, you will be left with your certificates and new `docker-compose.yaml` and `nginx.conf` files. Edit these to suit your project but be careful not to remove any of the `volumes` lines in the `docker-compose.yaml`, and don't remove the `ssl_cert*` lines in `nginx.conf`.
---
+++ certbot.py
@@ -0,0 +1,160 @@
+# Save this in a file called certbot.json and edit as required
+#
+# {
+#   "domain": "example.com",
+#   "volume_prefix": "./data",
+#   "email": "certbot@example.com"
+# }
+
+# Do not change anything below this line, unless you know what you are doing :)
+
+import json
+import os
+
+
+def cleanup():
+    print(f"\n\nDone. Navigate to https://{config['domain']} in your browser...")
+    if input(
+        f"\nRemove the setup files (if {config['domain']} has a valid certificate you don't need them anymore) (Y/n): "
+    ) in ["", "y", "Y"]:
+        os.remove("certbot.json")
+        os.remove("certbot.py")
+        print("Deleted the setup files.")
+    print("\nAdd the following line to your crontab to auto-renew this certificate:")
+    print("0 0,12 * * * docker compose run --rm certbot renew")
+    print(
+        "\nNow go ahead and edit the docker-compose.yml and nginx.conf files, being careful that you:"
+    )
+    print(" 1) don't remove any of the 'volumes' lines in docker-compose.yaml")
+    print(" 2) don't remove any of the 'ssl_cert*' lines in nginx.conf")
+
+
+def get_certificate(config, dry_run=True):
+    os.system(
+        f"docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ {'--dry-run' if dry_run else ''} -d {config['domain']} --non-interactive --agree-tos -m {config['email']}"
+    )
+
+
+def load_config():
+    f = open("certbot.json")
+    config = json.load(f)
+    f.close()
+
+    # TODO: validate the config, e.g.
+    # - domain is a valid fqdn
+    # - volume_prefix is a non-empty string
+    # - email is a valid email string
+
+    return config
+
+
+def start_docker():
+    os.system("docker compose up -d")
+
+
+def stop_docker():
+    if os.path.exists("docker-compose.yaml"):
+        os.system("docker compose down")
+
+
+def write_docker_compose_yml(config):
+    write_file(
+        "docker-compose.yml",
+        f"""version: '3'
+
+services:
+  webserver:
+    image: nginx:latest
+    ports:
+      - 80:80
+      - 443:443
+    restart: always
+    volumes:
+      - ./nginx.conf:/etc/nginx/conf.d/default.conf:ro
+      - {config['volume_prefix']}/certbot/www:/var/www/certbot/:ro
+      - {config['volume_prefix']}/certbot/conf/:/etc/nginx/ssl/:ro
+
+  certbot:
+    image: certbot/certbot:latest
+    volumes:
+      - {config['volume_prefix']}/certbot/www/:/var/www/certbot/:rw
+      - {config['volume_prefix']}/certbot/conf/:/etc/letsencrypt/:rw
+  """,
+    )
+
+
+def write_file(name, contents):
+    f = open(name, "w")
+    f.write(contents)
+    f.close()
+
+
+def write_post_nginx_conf(config):
+    write_file(
+        "nginx.conf",
+        f"""server {{
+    listen 80;
+    listen [::]:80;
+
+    server_name _;
+    server_tokens off;
+
+    location /.well-known/acme-challenge/ {{
+        root /var/www/certbot;
+    }}
+
+    location / {{
+        return 301 https://{config['domain']}$request_uri;
+    }}
+}}
+
+server {{
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name _;
+    server_tokens off;
+
+    ssl_certificate /etc/nginx/ssl/live/{config['domain']}/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/{config['domain']}/privkey.pem;
+
+    location / {{
+        if ( $host !~* ^({config['domain']})$ ) {{
+            return 444;
+        }}
+
+        root /usr/share/nginx/html;
+    }}
+}}""",
+    )
+
+
+def write_pre_nginx_conf(config):
+    write_file(
+        "nginx.conf",
+        """server {
+    listen 80 default_server;
+    listen [::]:80;
+
+    location /.well-known/acme-challenge/ {
+      root /var/www/certbot;
+    }
+
+    location / {
+      return 301 https://$host$request_uri;
+    }
+}""",
+    )
+
+
+if __name__ == "__main__":
+    config = load_config()
+    stop_docker()
+    write_docker_compose_yml(config)
+    write_pre_nginx_conf(config)
+    start_docker()
+    get_certificate(config, dry_run=False)
+    stop_docker()
+    write_post_nginx_conf(config)
+    start_docker()
+    cleanup()
